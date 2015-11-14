267f98a9f285406cdf0a7e4a001d8cf1ca2dfb2b
Adam Maynard <amaynar4@uwo.ca>
2015-11-14 03:52:38 +0000

    assignment 2

---
+++ client/client.c
@@ -0,0 +1,137 @@
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <syslog.h>
+#include <hdb.h>
+#include <hfs.h>
+#include <syslog.h>
+
+int main(int argc, char** argv)
+{
+
+  int c;
+  int verbose_flag = 0; 
+  char* log_filename = NULL; 
+  char* server_name = NULL;
+  char* server_port = NULL; 
+  char* server_root = NULL; 
+  char* username = NULL;
+  char* password = NULL; 
+  char* log_file = logfile; 
+
+  openlog(log_file, LOG_PID | LOF_NDELAY, LOG_USER); 
+
+  while(1){
+
+	static struct option long_options[]=
+	{
+
+	  {"server", required_argument, 0,'s'},
+	  {"port", required_argument, 0, 'p'},
+	  {"dir", required_argument, 0, 'd'},
+	  {"verbose", no_argument, &verbose_flag, 1},
+	  {0, 0, 0, 0}
+
+	};
+
+	int option_index = 0; 
+	c = getopt_long(argc, argv, "spdv:", long_options, &option_index);
+
+	if (c == -1)break;
+	
+	switch(c){
+
+	  case 's' :
+	  if (optarg == NULL){server_name = "localhost";}
+	  else{
+	    syslog(LOG_INFO, "servername option read");
+	    server_name = optarg; 
+	  }
+	  break;
+	
+	  case 'p' : 
+	  if (optarg == NULL) {server_port = "9000";}
+	  else {
+	    syslog(LOG_INFO, "serverport option read");
+	    server_port = optarg;
+	  }
+	  break; 
+
+	  case 'd' : 
+	  if (optarg == NULL){server_root = "~/hooli";}
+	  else {
+	    syslog(LOG_INFO, "server root address option read");
+	    server_root = optarg;
+	  }
+	  break;
+
+	  case 'v' :
+	  verbose_flag = true;    
+	  break;
+
+	} 
+
+  } 
+ 
+	if ((optind == argc - 2) && (argv[optind] != NULL)){
+
+  	  username = argv[optind];
+ 	  ++optind;
+
+	}//check for a username argument
+	else {
+	printf("invalid arguments");
+	exit(EXIT_FAILURE);
+	}
+
+	if ((optind == argc - 1) && (argv[optind] != NULL)){
+
+	  password = argv[optind];
+	  ++optind;	
+
+	}//check for a password argument
+	else{
+	printf ("invalid arguments");
+	exit(EXIT_FAILURE);
+	} 
+
+	hdb_connection* connection = hdb_connect(server_name); 
+	if (connection == NULL)
+	{
+
+	  printf("No connection.");
+	  exit(EXIT_FAILURE); 
+
+	}//check whether connnection went through 	
+
+	
+
+	char* auth = hdb_authentication(connection, username, password);
+	
+	if (auth == NULL)
+	{
+
+	  syslog(LOG_ERR,"credentials wrong.");
+	  exit(EXIT_FAILURE);
+
+	}//check credentials with server 
+
+	hfs_entry* clientList =  hfs_get_files (server_root); 
+	hdb_record* serverList = hdb_user_files(connection, username);
+
+	if (clientList == NULL){
+	  syslog(LOG_ERR,"no client files");
+	}
+	if (serverList == NULL){
+	syslog(LOG_ERR,"no server files");
+	}
+
+	while(list != NULL){
+
+	  //stuff
+
+	  list = list->next;//iterate
+
+	}
+
+}
